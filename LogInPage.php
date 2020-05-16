7f4bc4b00dbb0ebd9b47c713c15a6a82cacfd63b
c712g285 <54552684+c712g285@users.noreply.github.com>
2020-05-15 20:55:38 -0500

    1
    1

--- LogInPage.php
+++ LogInPage.php
@@ -4,20 +4,26 @@ session_start();
 <html lang="en" dir="ltr">
   <head>
     <meta charset="utf-8">
+    <link href="LogInPage.css" rel="stylesheet" type="text/css">
     <title>STEM Book</title>
-    <center>
-      <h1>Login to your STEM Book</h1>
-    </center>
   </head>
   <body>
     <center>
       <form action="CheckLogin.php" method="post">
-        <p>Nickname: <input type="text" name="username" ></p>
-        <p>Password: <input type="text" name="password" ></p>
-        <input type="submit"  value="Login">
+        <div class="container">
+          <h1>Login to your STEM Book</h1>
+          <hr>
+          <label for="username"><b>User name</b></label>
+          <input type="text" placeholder="Enter username" name="username" required>
+
+          <label for="psw"><b>Password</b></label>
+          <input type="password" placeholder="Enter Password" name="password" required>
+          <hr>
+          <button type="submit" class="LogInBtn">Log In</button>
+        </div>
       </form>
       <p> Create Your Acount now!</p>
-      <a href='Account/CreateAccount.html'><button>Register Here</button></a>
+      <a href='Account/CreateAccount.html'><button class="button">Register Here</button></a>
     </center>
   </body>
 </html>
